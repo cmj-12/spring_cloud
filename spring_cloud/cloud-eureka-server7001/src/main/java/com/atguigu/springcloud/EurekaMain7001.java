26592fb177be44a15d01897747a5d38218b7db58
cuiminjie <1178105479@qq.com>
2022-07-25 22:56:12 +0800

    订单和支付注册到eureka上

--- spring_cloud/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ spring_cloud/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author cuiminjie
- * @Date 2022 07 21 21 52
+ * @Date 2022 07 25 22 16
  **/
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
--- spring_cloud/cloud-provide-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ spring_cloud/cloud-provide-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author cuiminjie
  * @Date 2022 07 19 21 08
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
